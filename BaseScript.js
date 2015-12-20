cfee76298b58035ebe092fd1b77a51deb31e710d
KamioMasakazu <kamio.masakazu@gmail.com>
2015-12-20 16:55:13 +0900

    表示モードでのソートを追加

--- BaseScript.js
+++ BaseScript.js
@@ -18,6 +18,7 @@ var KNameCard = KNameCard || {
 		{"Name":"EMail",		"Title":"EMail"},
 		{"Name":"HomePage",		"Title":"ホームページ"},
 	],
+	SortOrder:{},
 };
 
 var electron = require('electron');
@@ -37,7 +38,7 @@ KNameCard.readConfigFile = function(){
 	var config_str = fs.readFileSync(app.getPath('userData') + '/config.json', 'utf-8');
 	KNameCard.Config = JSON.parse(config_str);
 	//console.log(KNameCard.Config);
-	
+
 	return ;
 }
 
@@ -60,17 +61,17 @@ KNameCard.checkNameCardFormat = function(data){
 	if ((!data) || (!data.Discription) || (!data.CardList)) {
 		return  false;
 	}
-	
+
 	// チェック文字列が無い
 	if (data.Discription != "NameCardList") {
 		return  false;
 	}
-	
+
 	// 名刺リストが配列で無い
 	if (!Array.isArray(data.CardList)) {
 		return false;
 	}
-	
+
 	return true;
 }
 
@@ -85,7 +86,7 @@ KNameCard.checkNameCard = function(data){
 			"Discription": "NameCardList",
 			"CardList": [],
 	};
-	
+
 	if (KNameCard.checkNameCardFormat(data)) {
 		return data;
 	}
@@ -104,18 +105,18 @@ KNameCard.readNameCards = function(file){
 	if (!file) {
 			file = KNameCard.Config.DataPath;
 	}
-	
+
 	var data;
 	try{
 		var list = fs.readFileSync(file, 'utf-8');
-		data = JSON.parse(list);	
+		data = JSON.parse(list);
 	}
 	catch(e){
 		data = null;
 	}
-	
+
 	KNameCard.Cards = KNameCard.checkNameCard(data);
-	
+
 	return;
 }
 
@@ -126,7 +127,7 @@ KNameCard.writeNameCards = function(file){
 	if (!file) {
 		file = KNameCard.Config.DataPath;
 	}
-	
+
 	var data = JSON.stringify(KNameCard.Cards, null, '\t');
 	fs.writeFileSync(file, data, null, function (err) {
 	    console.log(err);
@@ -144,14 +145,14 @@ KNameCard.addDelButtonToRow = function(tr){
 	// 削除ボタン
 	var del_td = document.createElement("td");
 	var del_btn = document.createElement("button");
-	
+
 	del_btn.innerHTML = "×";
 	del_btn.onclick = KNameCard.delAndShow;
-	
+
 	del_td.appendChild(del_btn);
 	tr.appendChild(del_td);
-	
-	return;	
+
+	return;
 }
 
 /**
@@ -160,31 +161,39 @@ KNameCard.addDelButtonToRow = function(tr){
  */
 KNameCard.createListTitle = function(mode){
 	var title = document.getElementById('NameCardHeader');
-	
+
 	for(var i=title.childNodes.length-1; i>=0; i--){
 		title.removeChild(title.childNodes[i]);
 	}
 	var tr = document.createElement("tr");
-	
+
 	// 削除欄を追加
 	if (mode == 'Input') {
 		var del_td = document.createElement("th");
 		tr.appendChild(del_td);
 	}
-	
+
 	for(var i=0; i<KNameCard.EntryList.length; i++){
 		var col = KNameCard.EntryList[i].Name;	// 設定項目
-		
+
 		// 表示モードの時は表示対象項目だけを表示する
 		if ((mode == 'View') && (KNameCard.Config.DispCols.indexOf(col) < 0)) {
 			continue;
 		}
-		
-		var td = document.createElement("th");
-		td.innerHTML = KNameCard.EntryList[i]["Title"];
-		tr.appendChild(td);
+
+		var th = document.createElement("th");
+		th.innerHTML = KNameCard.EntryList[i]["Title"];
+
+		// 表示モードの時はソート用のハンドラなどを設定する
+		if(mode == 'View'){
+			th.setAttribute("id", "Col" + col);
+			th.style.cursor = "pointer";
+			th.onclick = KNameCard.clickToSort;
+		}
+
+		tr.appendChild(th);
 	}
-	
+
 	title.appendChild(tr);
 	return;
 }
@@ -197,23 +206,23 @@ KNameCard.createListTitle = function(mode){
 KNameCard.createCardEntryView = function(id, value){
 	var tr = document.createElement("tr");
 	tr.setAttribute("id", "CardNo"+id);
-	
+
 	// 項目
 	for(var i=0; i<KNameCard.EntryList.length; i++){
 		var col = KNameCard.EntryList[i].Name;	// 設定項目
-		
+
 		// 表示対象項目だけを表示する
 		if (KNameCard.Config.DispCols.indexOf(col) < 0) {
 			continue;
 		}
-		
+
 		var td = document.createElement("td");
 		td.setAttribute("Class", col);
-		
+
 		var val = value[col];
 		var txt = document.createTextNode(val);
 		td.appendChild(txt);
-		
+
 		// 地図用ボタン
 		if ((col == "Address1") && (val)) {
 			var btn = document.createElement("button");
@@ -223,10 +232,10 @@ KNameCard.createCardEntryView = function(id, value){
 			btn.onclick = KNameCard.clickMapButton;
 			td.appendChild(btn);
 		}
-		
+
 		tr.appendChild(td);
 	}
-	
+
 	return tr;
 }
 
@@ -238,20 +247,20 @@ KNameCard.createCardEntryView = function(id, value){
 KNameCard.createCardEntryInput = function(id, value){
 	var tr = document.createElement("tr");
 	tr.setAttribute("id", "CardNo"+id);
-	
+
 	// 削除ボタン追加
 	KNameCard.addDelButtonToRow(tr);
-	
+
 	// 項目
 	for(var i=0; i<KNameCard.EntryList.length; i++){
 		var col = KNameCard.EntryList[i].Name;	// 設定項目
-		
+
 		var td = document.createElement("td");
 		td.setAttribute("Class", col);
 		var input = document.createElement("input");
 		input.setAttribute("type", "text");
 		input.setAttribute("class", "ListInput " + col);
-		
+
 		var val = value[col];
 		var txt = document.createTextNode(val);
 		input.appendChild(txt);
@@ -260,7 +269,7 @@ KNameCard.createCardEntryInput = function(id, value){
 		td.appendChild(input);
 		tr.appendChild(td);
 	}
-	
+
 	return tr;
 }
 
@@ -273,13 +282,13 @@ KNameCard.createList = function(mode, data){
 	if (!data) {
 		data = KNameCard.Cards.CardList;
 	}
-	
+
 	var list = document.getElementById('NameCardBody');
-	
+
 	for(var i=list.childNodes.length-1; i>=0; i--){
 		list.removeChild(list.childNodes[i]);
 	}
-	
+
 	for(var i=0; i<data.length; i++){
 		var tr;
 		if (mode == 'Input') {
@@ -290,7 +299,7 @@ KNameCard.createList = function(mode, data){
 		}
 		list.appendChild(tr);
 	}
-	
+
 	return;
 }
 
@@ -300,28 +309,28 @@ KNameCard.createList = function(mode, data){
  */
 KNameCard.appendInputField = function(){
 	var id = KNameCard.Cards.CardList.length;
-	
+
 	var list = document.getElementById('NameCardBody');
 	var tr = document.createElement("tr");
 	tr.setAttribute("id", "CardNo"+id);
-	
+
 	// 削除ボタン追加
 	KNameCard.addDelButtonToRow(tr);
-	
+
 	for(var i=0; i<KNameCard.EntryList.length; i++){
 		var col = KNameCard.EntryList[i].Name;	// 設定項目
-		
+
 		var td = document.createElement("td");
 		td.setAttribute("Class", col);
 		var input = document.createElement("input");
 		input.setAttribute("type", "text");
 		input.setAttribute("class", "ListInput " + col);
 		input.onchange = KNameCard.changeInput;
-		
+
 		td.appendChild(input);
 		tr.appendChild(td);
 	}
-	
+
 	list.appendChild(tr);
 	return;
 }
@@ -332,25 +341,25 @@ KNameCard.appendInputField = function(){
 KNameCard.changeInput = function(event){
 	var elm = event.target;
 	var val = elm.value.trim();	// 入力された値
-	
+
 	// 入力値がなければ無視
 	if (!val) {
 		return;
 	}
-	
+
 	// <tr>のIDから要素番号を作る
 	var pos_flg = elm.parentNode.parentNode.id;
 	var id = parseInt(pos_flg.replace("CardNo", ""));
-	
+
 	// 新規入力時
 	if (id == KNameCard.Cards.CardList.length) {
 		KNameCard.addNewEntry();
 		KNameCard.appendInputField();
 	}
-	
+
 	var key = elm.classList[1];
 	KNameCard.Cards.CardList[id][key] = val;
-	
+
 	return;
 }
 
@@ -373,7 +382,7 @@ KNameCard.addNewEntry = function(){
 			"EMail":"",
 			"HomePage":""
 	}
-	
+
 	KNameCard.Cards.CardList.push(obj);
 	return;
 }
@@ -397,7 +406,7 @@ KNameCard.delAndShow = function(event){
 	var elm = event.target;
 	var pos_flg = elm.parentNode.parentNode.id;
 	var id = parseInt(pos_flg.replace("CardNo", ""));
-	
+
 	KNameCard.delEntry(id);
 	KNameCard.showNameCardList();
 }
@@ -407,10 +416,10 @@ KNameCard.delAndShow = function(event){
  */
 KNameCard.showNameCardList = function(){
 	var mode = KNameCard.DispMode;
-	
+
 	KNameCard.createListTitle(mode);
 	KNameCard.createList(mode);
-	
+
 	if (mode == 'Input') {
 		KNameCard.appendInputField();
 		KNameCard.showConfigArea(false);
@@ -421,6 +430,47 @@ KNameCard.showNameCardList = function(){
 	else{
 		KNameCard.showConfigArea(false);
 	}
+}
+
+/******************************************************************************
+ *	ソート関連
+ *****************************************************************************/
+
+/**
+ *	以前のソート順をチェックして今回のソート順を返す
+ *	col：ソート項目
+ */
+KNameCard.getSortOrder = function(col){
+	if(col in KNameCard.SortOrder){
+		KNameCard.SortOrder[col] *= -1;
+	}
+	else{
+		KNameCard.SortOrder[col] = 1;
+	}
+	return KNameCard.SortOrder[col];
+}
+
+/**
+ *	ソートして表示する
+ */
+KNameCard.clickToSort = function(event){
+	var elm = event.target;
+	var col = elm.id.replace("Col", "");
+
+	var order = KNameCard.getSortOrder(col);
+	var list = KNameCard.Cards.CardList.concat();
+	list.sort(function(a, b){
+		if(a[col] > b[col]){ return order;}
+		else if(a[col] < b[col]){ return (-1 * order)}
+		else{return 0;}
+	});
+
+	var mode = KNameCard.DispMode;
+
+	KNameCard.createListTitle(mode);
+	KNameCard.createList(mode, list);
+
+	return;
 }
 
 /******************************************************************************
@@ -438,7 +488,7 @@ KNameCard.checkFilter = function(name_card, str){
 	if (!str) {
 		return true;
 	}
-	
+
 	var target = "";
 	for(var key in name_card){
 		var val = name_card[key];
@@ -448,7 +498,7 @@ KNameCard.checkFilter = function(name_card, str){
 		}
 		target += val;
 	}
-	
+
 	if (target.indexOf(str) >= 0) {
 		return true;
 	}
@@ -466,9 +516,9 @@ KNameCard.getTargetNameList = function(filter){
 	if (!filter || !filter[0]) {
 		return null;
 	}
-	
+
 	var list = KNameCard.Cards.CardList.concat();
-	
+
 	for(var i=0; i<filter.length; i++){
 		for(var j=list.length-1; j>=0; j--){
 			if (KNameCard.checkFilter(list[j], filter[i]) == false) {
@@ -476,7 +526,7 @@ KNameCard.getTargetNameList = function(filter){
 			}
 		}
 	}
-	
+
 	return list;
 }
 
@@ -487,13 +537,13 @@ KNameCard.getTargetNameList = function(filter){
 KNameCard.searchCardList = function(){
 	var input = document.getElementById("KeyWords").value.trim();
 	var keywords = input.split(/\s+/);
-	
+
 	var list = KNameCard.getTargetNameList(keywords);
 	var mode = KNameCard.DispMode;
-	
+
 	KNameCard.createListTitle(mode);
 	KNameCard.createList(mode, list);
-	
+
 	return;
 }
 
@@ -504,24 +554,24 @@ KNameCard.showDisplayColConfig = function(){
 	var ConfigShowCols = document.getElementById("ConfigShowCols");
 	var ShowColsCheckBoxs = document.getElementById("ShowColsCheckBoxs");
 	ShowColsCheckBoxs.innerHTML = "";
-	
+
 	for(var i=0; i<KNameCard.EntryList.length; i++){
 		var div = document.createElement("div");
 		div.setAttribute("class", "CheckBoxDiv");
-		
+
 		var cb = document.createElement("input");
 		cb.setAttribute("type", "checkbox");
 		cb.setAttribute("name", "DispColConfig");
 		cb.value = KNameCard.EntryList[i]["Name"];
-		
+
 		if (KNameCard.Config.DispCols.indexOf(KNameCard.EntryList[i]["Name"]) >= 0) {
 			cb.checked = true;
 		}
-		
+
 		div.appendChild(cb);
 		var txt = document.createTextNode(KNameCard.EntryList[i]["Title"]);
 		div.appendChild(txt);
-		
+
 		ShowColsCheckBoxs.appendChild(div);
 	}
 	return;
@@ -664,10 +714,10 @@ KNameCard.open = function(){
 				else{
 					filename = files[0];
 				}
-				
+
 				var list = fs.readFileSync(filename, 'utf-8');
 				var data = JSON.parse(list);
-				
+
 				if(KNameCard.checkNameCardFormat(data)){
 					KNameCard.Cards = data;
 					KNameCard.showNameCardList();
@@ -700,6 +750,10 @@ KNameCard.saveWindowSize = function(){
     }, 1000);
 }
 
+/******************************************************************************
+ *	コピー、ペースト
+ *****************************************************************************/
+
 /**
  *	コピー
  */
@@ -719,11 +773,11 @@ KNameCard.paste = function(){
 	if (elm.tagName != "INPUT") {
 		return;
 	}
-	
+
 	if (elm.type.toUpperCase() != "TEXT") {
 		return;
 	}
-	
+
 	elm.value = clipboard.readText();
 }
 
@@ -737,23 +791,23 @@ KNameCard.paste = function(){
  */
 KNameCard.drawMap = function(addr){
 	var MapArea = document.getElementById("MapArea");
-	
+
 	var geocoder = new google.maps.Geocoder();
 	var lat = 0;
 	var lng = 0;
 	geocoder.geocode({'address': addr, 'language': 'ja'}, function(results, status) {
 		lat = results[0].geometry.location.lat();
 		lng = results[0].geometry.location.lng();
-		
+
 		// 中心の位置座標を指定する
 		var Center = new google.maps.LatLng( lat , lng );
-		
+
 		// 地図のオプションを設定する
 		var mapOptions = {
 		   zoom: 16 ,				// ズーム値
 		   center: Center ,		// 中心座標 [latlng]
 		};
-		
+
 		// [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
 		var map = new google.maps.Map( MapArea , mapOptions ) ;
 
@@ -773,11 +827,11 @@ KNameCard.drawMap = function(addr){
 KNameCard.displayMapArea = function(){
 	var w = 0.6 * window.innerWidth;
 	var h = 0.6 * window.innerHeight;
-	
+
 	var map_area = document.getElementById("MapArea");
 	map_area.style.width = w + "px";
 	map_area.style.height = h + "px";
-	
+
 	var map_wrapper = document.getElementById("MapWrapper");
 	map_wrapper.style.display = "block";
 }
@@ -790,7 +844,7 @@ KNameCard.clickMapButton = function(event){
 	if (!btn.value) {
 		return;
 	}
-	
+
 	KNameCard.displayMapArea();
 	KNameCard.drawMap(btn.value);
 }
@@ -800,5 +854,5 @@ KNameCard.clickMapButton = function(event){
  */
 KNameCard.closeMap = function(){
 	var map_wrapper = document.getElementById("MapWrapper");
-	map_wrapper.style.display = "none";	
+	map_wrapper.style.display = "none";
 }
